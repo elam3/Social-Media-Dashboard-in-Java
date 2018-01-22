cf82613caa7ded200ed47959ce0102f9de12f2da
Edison L <elam3@mail.ccsf.edu>
2018-01-21 18:04:28 -0800

    added an interactive main menu

--- DriverProgram.java
+++ DriverProgram.java
@@ -4,52 +4,64 @@ public class DriverProgram
 {
     public static void main (String [] args)
     {
-        String author;
-        String content;
-        String response;
         ArrayList <Post> posts = new ArrayList <Post> ();
+        String menuSelect;
 
         do
         {
-            //Add New Post
-            System.out.println("Would you like to post on the social media site? (Y/N)");
-            response = Post.scan.nextLine();
-            if (response.equalsIgnoreCase("y"))
+            displayMainMenu();
+
+            menuSelect = Post.scan.nextLine();
+            if (menuSelect.equalsIgnoreCase("p"))
             {
-                System.out.println("Name: ");
-                author = Post.scan.nextLine();
-                System.out.println("Message content:");
-                content = Post.scan.nextLine();
-                System.out.println("What site would you like to post on?" +
-                        "\n1 for Facebook" +
-                        "\n2 for Instagram" +
-                        "\n3 for Twitter ");
-                int siteNumberAnswer = Integer.parseInt(Post.scan.nextLine());
-                switch (siteNumberAnswer)
-                {
-                    case 1:
-                        //Create Facebook Post
-                        makeNewFacebookPost(posts, author, content);
-                        break;
-                    case 2:
-                        //Create Instagram Post
-                        makeNewInstagramPost(posts, author, content);
-                        break;
-                    case 3:
-                        //Create Twitter Post
-                        makeNewTwitterPost(posts, author, content);
-                        break;
-                    default:
-                        //Default Post
-                        break;
-                }
-            }//"Would you like to post on the social media site? (Y/N)";
+                //Print all posts
+                printPosts(posts);
+            }
+            else if (menuSelect.equalsIgnoreCase("a"))
+            {
+                //Add New Post
+                addNewPost(posts);
+            }
+            else if (menuSelect.equalsIgnoreCase("i"))
+            {
+                //Interact with posts
+                interactWithPosts(posts);
+            }
+            else if (menuSelect.equalsIgnoreCase("d"))
+            {
+                //Delete a post
+                deleteLocalPost(posts);
+            }
+            else if (menuSelect.equalsIgnoreCase("cmp"))
+            {
+                //Compare two posts
+                comparePosts(posts);
+            }
+
         }
-        while(response.equalsIgnoreCase("y"));
+        while(!menuSelect.equalsIgnoreCase("q"));
+    }//main()
 
-        //Print posts
-        displayPosts(posts);
+    public static void displayMainMenu()
+    {
+        System.out.println("What would you like to do?");
+        System.out.println(
+              "[   p ] Print all posts.\n"
+            + "[   a ] Add a new post.\n"
+            + "[   i ] Interact with posts.\n"
+            + "[   d ] Delete a post.\n"
+            + "[ cmp ] Compare two posts.\n"
+            + "[   q ] Quit.\n");
+    }
 
+    public static void comparePosts(ArrayList<Post> posts)
+    {
+        //Test post comparisons
+        System.out.println(posts.get(0).equals(posts.get(1)));
+    }
+
+    public static void interactWithPosts(ArrayList<Post> posts)
+    {
         //Interact with posts
         for(Post p : posts)
         {
@@ -66,20 +78,58 @@ public class DriverProgram
                 ((InstagramPost) p).sendToFriend();
             }
         }
+    }
 
+    public static void deleteLocalPost(ArrayList<Post> posts)
+    {
         //Test deleting posts
         posts.get(0).deletePost();
         posts.remove(0);
+    }
 
-        //Print posts after changes
-        displayPosts(posts);
+    public static void addNewPost(ArrayList<Post> posts)
+    {
+        String author;
+        String content;
+        String response;
 
-        //Test post comparisons
-        System.out.println(posts.get(0).equals(posts.get(1)));
-    }//main()
+        //Add New Post
+        System.out.println("Would you like to post on the social media site? (Y/N)");
+        response = Post.scan.nextLine();
+        if (response.equalsIgnoreCase("y"))
+        {
+            System.out.println("Name: ");
+            author = Post.scan.nextLine();
+            System.out.println("Message content:");
+            content = Post.scan.nextLine();
+            System.out.println("What site would you like to post on?" +
+                    "\n1 for Facebook" +
+                    "\n2 for Instagram" +
+                    "\n3 for Twitter ");
+            int siteNumberAnswer = Integer.parseInt(Post.scan.nextLine());
+            switch (siteNumberAnswer)
+            {
+                case 1:
+                    //Create Facebook Post
+                    makeNewFacebookPost(posts, author, content);
+                    break;
+                case 2:
+                    //Create Instagram Post
+                    makeNewInstagramPost(posts, author, content);
+                    break;
+                case 3:
+                    //Create Twitter Post
+                    makeNewTwitterPost(posts, author, content);
+                    break;
+                default:
+                    //Default Post
+                    break;
+            }
+        }//"Would you like to post on the social media site? (Y/N)";
+    }
 
     //Display Posts
-    public static void displayPosts(ArrayList<Post> posts)
+    public static void printPosts(ArrayList<Post> posts)
     {
         int count = 0;
         for(Post p : posts)
