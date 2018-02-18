a8e3ab69a8b42ed7a538ea4dc44efb5eba602dd1
Edison L <elam3@mail.ccsf.edu>
2018-02-18 15:29:39 -0800

    added light documentation to GUIView

--- GUIView.java
+++ GUIView.java
@@ -1,33 +1,40 @@
-import java.time.*;
-import java.util.*;
-import javafx.application.*;
-import javafx.stage.*;
-import javafx.scene.*;
-import javafx.scene.paint.*;
 import javafx.scene.layout.*;
-import javafx.scene.image.*;
 import javafx.scene.control.*;
-import javafx.geometry.*;
 
+//TODO: rename this class? e.g. TableOfPosts ?
 public class GUIView {
     
     private ScrollPane tableViewSP;
     private VBox tableView;
+    private final static int V_GAP = 20;
 
+    /**
+     * Make a table of cells, and attach it to the
+     * pane that's passed in.
+     */
     public GUIView(Pane pane) {
+        // + Pane -----------------
+        // | + Scroll Pane --------
+        // | | + VBox -------------
+        // | | | | + CellView -----
+        // | | | | + CellView -----
+        // | | | | + ...more CellView
+
         tableViewSP = new ScrollPane();
-            tableViewSP.setPrefWidth(600);
-            tableViewSP.prefHeightProperty().bind(
-                pane.heightProperty()
-            );
+        tableViewSP.prefWidthProperty().bind(pane.widthProperty());
+        tableViewSP.prefHeightProperty().bind(pane.heightProperty());
             tableView = new VBox();
             tableView.getStyleClass().add("tableView");
-            tableView.setSpacing(20);
+            tableView.setSpacing(V_GAP);
         tableViewSP.setContent(tableView);
+
         pane.getChildren().add(tableViewSP);
     }
 
     //TODO: is this allowed in a View?
+    /**
+     * Attaches new cells to the tableView
+     */
     public void add(CellView cellView) {
         tableView.getChildren().add(cellView.getParent());
     }
